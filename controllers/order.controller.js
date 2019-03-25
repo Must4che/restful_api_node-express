c192023e34a1d0d8708f5d4c76d87b0bfbca5508
Must4che <marek.tuska116@gmail.com>
2019-03-25 07:27:26 +0100

    refactored old then-catch to async-await

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -3,103 +3,99 @@ const mongoose = require('mongoose');
 const Order = require('../models/order.model');
 const Product = require('../models/product.model');
 
-exports.getAllOrders = (req, res, next) => {
-    Order.find()
-        .select('product quantity _id')
-        .populate('product', 'name price') // without populate there will be only _id populated not the whole product
-        .exec()
-        .then( response => {
-            res.status(200).json({
-                count: response.length,
-                orders: response.map( item => {
-                    return {
-                        _id: item._id,
-                        product: item.product,
-                        quantity: item.quantity,
-                        request: {
-                            type: 'GET',
-                            url: 'http://localhost:1234/orders/' + item._id
-                        }
+exports.getAllOrders = async (req, res, next) => {
+    try {
+        const orders = await Order
+            .find()
+            .select('product quantity _id')
+            .populate('product', 'name price') // without populate there will be only _id populated not the whole product
+            .exec();  
+        res.status(200).json({
+            count: orders.length,
+            orders: orders.map( item => {
+                return {
+                    _id: item._id,
+                    product: item.product,
+                    quantity: item.quantity,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:1234/orders/' + item._id
                     }
-                })
-            });
-        })
-        .catch( err => {
-            res.status(500).json({ error: err });
+                }
+            })
         });
+    } catch( err ) {
+        res.status(500).json({ error: err });
+    }
 }
 
-exports.getOrderById = (req, res, next) => {
-    Order.findById(req.params.orderId)
-        .populate('product', 'name price')
-        .exec()
-        .then( order => {
-            if ( !order ) {
-                return res.status(404).json({ message: "Order not found." });
+exports.getOrderById = async (req, res, next) => {
+    try {
+        const order = await Order
+            .findById(req.params.orderId)
+            .populate('product', 'name price')
+            .exec();
+        if ( !order ) {
+            return res.status(404).json({ message: "Order not found." });
+        }
+        res.status(200).json({
+            order: order,
+            request: {
+                type: 'GET',
+                url: 'http://localhost:1234/orders/'
             }
-            res.status(200).json({
-                order: order,
-                request: {
-                    type: 'GET',
-                    url: 'http://localhost:1234/orders/'
-                }
-            });
-        })
-        .catch( err => {
-            res.status(500).json({ error: err });
         });
+    } catch ( err ) {
+        res.status(500).json({ error: err });
+    }
 }
 
-exports.createOrder = (req, res, next) => {
-    Product.findById(req.body.productId)
-        .then( product => {
-            if ( !product ) {
-                return res.status(404).json({ message: "Product not found." });
-            } 
-            const order = new Order({
-                _id: new mongoose.Types.ObjectId(),
-                quantity: req.body.quantity,
-                product: req.body.productId
-            });
-            return order.save(); 
-        })
-        .then( response =>{
-            console.log(response);
-            res.status(201).json({
-                message: `Order created with Id: ${response._id}`,
-                createdOrder: {
-                    _id: response._id,
-                    product: response.product,
-                    quantity: response.quantity
-                },
-                request: {
-                    type: 'GET',
-                    url: 'http://localhost:1234/orders/' + response._id
-                }
-            });
-        })
-        .catch( err => {
-            res.status(500).json({ error: err });
-    });     
+exports.createOrder = async (req, res, next) => {
+    try {
+        const product = await Product.findById(req.body.productId);
+        if ( !product ) {
+            return res.status(404).json({ message: "Product not found." });
+        }
+        const order = new Order({
+            _id: new mongoose.Types.ObjectId(),
+            quantity: req.body.quantity,
+            product: req.body.productId
+        });
+        const response = await order.save();
+        res.status(201).json({
+            message: `Order created with Id: ${response._id}`,
+            createdOrder: {
+                _id: response._id,
+                product: response.product,
+                quantity: response.quantity
+            },
+            request: {
+                type: 'GET',
+                url: 'http://localhost:1234/orders/' + response._id
+            }
+        });
+    } catch( err ) {
+        res.status(500).json({ error: err });
+    }
 }
 
-exports.deleteOrderById = (req, res, next) => {
-    Order.remove({ _id: req.params.orderId })
-        .exec()
-        .then( response => {
-            res.status(200).json({
-                message: `Order with Id: ${req.params.orderId} has been deleted`,
-                request: {
-                    type: 'GET',
-                    url: 'http://localhost:1234/orders/',
-                    body: {
-                        productId: 'Enter_id_here',
-                        quantity: 'Enter_quantity_here'
-                    }
+exports.deleteOrderById = async (req, res, next) => {
+    try {
+        await Order
+            .remove({ _id: req.params.orderId })
+            .exec();
+        res.status(200).json({
+            message: `Order with Id: ${req.params.orderId} has been deleted`,
+            request: {
+                type: 'GET',
+                url: 'http://localhost:1234/orders/',
+                body: {
+                    productId: 'Enter_id_here',
+                    quantity: 'Enter_quantity_here'
                 }
-            });
-        })
-        .catch( err => {
-            res.status(500).json({ error: err });
+            }
         });
+    } catch( err ) {
+        res.status(500).json({ error: err });
+    }
 }
